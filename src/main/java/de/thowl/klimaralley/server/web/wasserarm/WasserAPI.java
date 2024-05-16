adc7a5b6732da6bab35e127957ba933b7ca18259
j0giwa <77389555+j0giwa@users.noreply.github.com>
2024-05-16 16:49:48 +0200

    Documented unfinisched API methods

--- src/main/java/de/thowl/klimaralley/server/web/wasserarm/WasserAPI.java
+++ src/main/java/de/thowl/klimaralley/server/web/wasserarm/WasserAPI.java
@@ -14,6 +14,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 import lombok.extern.slf4j.Slf4j;
 
+
+/**
+ * API is a stub and subject to change.
+ */
 @Slf4j
 @RestController
 @RequestMapping("/water")
@@ -23,15 +27,53 @@ public class WasserAPI {
 	/**
 	 *  
 	 */
+	@Operation(summary = "Healthcheck")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Can be used to check if api is up", content = @Content),
+	})
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ResponseEntity<Object> test() {
+		log.info("entering getTask (GET-Method: /water/get)");
+		return ResponseEntity.status(HttpStatus.OK).body("hello world");
+	}
+
+	/**
+	 *  
+	 */
 	@Operation(summary = "Retrieves all shop items from the Database")
 	@ApiResponses(value = {
-		@ApiResponse(responseCode = "200", description = "Wasserarmsatt shopitems as json", content = @Content),	
+		@ApiResponse(responseCode = "501", description = "Wasserarmsatt shopitems as json", content = @Content),	
 	})
-	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	@RequestMapping(value = "/items", method = RequestMethod.GET)
 	public ResponseEntity<Object> getAllItems() {
-		log.info("entering getTask (GET-Method: /api/task/get)");
+		log.info("entering getAllItems (GET-Method: /water/items)");
+		return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body("Not implemented");
+	}
 
-		return ResponseEntity.status(HttpStatus.OK).body("hello world");
+	/**
+	 *  
+	 */
+	@Operation(summary = "Retrieves all shop items from the Database")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "501", description = "Wasserarmsatt score", content = @Content),
+	})
+	@RequestMapping(value = "/get/score", method = RequestMethod.GET)
+	public ResponseEntity<Object> getScore() {
+		log.info("entering getScore (GET-Method: /water/score)");
+		return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body("Not implemented");
+	}
+
+	/**
+	 *  
+	 */
+	@Operation(summary = "Retrieves all shop items from the Database")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "501", description = "Submit cart items", content = @Content),
+	})
+	@RequestMapping(value = "/items", method = RequestMethod.POST)
+	public ResponseEntity<Object> setCartItems() {
+		log.info("entering setCartItems (POST-Method: /water/items)");
+		return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body("Not implemented");
 	}
 
 }
