3d89c7b7f2b27b3e730ee185c83666a410ca0a78
j0giwa <77389555+j0giwa@users.noreply.github.com>
2024-05-26 21:00:49 +0200

    Updated documentation

--- src/main/java/de/thowl/klimaralley/server/web/api/auth/AuthAPI.java
+++ src/main/java/de/thowl/klimaralley/server/web/api/auth/AuthAPI.java
@@ -17,12 +17,15 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * RESTful API of the Wasserarm satt game.
- * This API is a stub and subject to change.
+ * RESTful API for Authentication.
+ *
+ * @author Jonas Schwind
+ * @version 1.0.0
  */
 @Slf4j
 @RestController
@@ -35,21 +38,28 @@ public class AuthAPI {
 
 	/**
 	 * Performs a login action
+	 *
+	 * @param schema {@link LoginSchema} contained in the resqeust
+	 *
+	 * @return JWT token (status: {@code 200 OK}) on successfull login,
+	 *	   {@code 201 UNAUTHORIZED} on unsuccessfull login
 	 */
 	@Operation(summary = "login")
 	@ApiResponses(value = {
-		@ApiResponse(responseCode = "200", description = "JSON Web Token", content = @Content),
-		@ApiResponse(responseCode="401",description="Invalid credentials",content=@Content),
+		@ApiResponse(responseCode = "200", description = "Successful operation",
+			content = @Content(mediaType = "text/plain",
+				schema = @Schema(implementation = String.class))),
+		@ApiResponse(responseCode = "401", description = "Invalid credentials"),
 	})
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public ResponseEntity<Object> doLogin(LoginSchema form) {
+	public ResponseEntity<Object> doLogin(LoginSchema schema) {
 
 		String email, password, token;
 
 		log.info("entering doLogin (POST-Method: /login)");
 
-		email = form.getEmail();
-		password = form.getPassword();
+		email = schema.getEmail();
+		password = schema.getPassword();
 
 		try {
 			token = authsvc.login(email, password);
@@ -62,14 +72,18 @@ public class AuthAPI {
 
 	/**
 	 * Registers a new user.
+	 *
+	 * @param schema {@link LoginSchema} contained in the resqeust
 	 * 
-	 * @return to register page
+	 * @return {@code 200 OK} on Register,
+	 *         {@code 400 BAD REQUEST} on invalid credentials,
+	 *         {@code 500 INTERNAL SERVER ERROR} if the user exists.
 	 */
 	@Operation(summary = "register")
 	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "null", content = @Content),
-			@ApiResponse(responseCode = "401", description = "Invalid credentials", content = @Content),
-			@ApiResponse(responseCode = "500", description = "User already exists", content = @Content),
+		@ApiResponse(responseCode = "200", description = "User Registered"),
+		@ApiResponse(responseCode = "400", description = "Invalid credentials"),
+		@ApiResponse(responseCode = "500", description = "User already exists"),
 	})
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	public ResponseEntity<Object> doRegister(RegisterSchema form) {
@@ -77,26 +91,25 @@ public class AuthAPI {
 		log.info("entering doRegister (POST-Method: /register)");
 
 		if (!authsvc.validateEmail(form.getEmail())) {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials"); 
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid Email"); 
 		}
 
 		if (!authsvc.validatePassword(form.getPassword())) {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid Password");
 		}
 
 		if (!form.getPassword().equals(form.getVerifyPassword())) {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Password don't match");
 		}
 
 		try {
 			this.authsvc.register(form.getFirstname(), form.getLastname(), form.getUsername(),
-					form.getEmail(),
-					form.getPassword());
+					form.getEmail(), form.getPassword());
 		} catch (DuplicateUserException e) {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("User already exists");
 		}
 
-		return ResponseEntity.status(HttpStatus.OK).body(null);
+		return ResponseEntity.status(HttpStatus.OK).body("User Registered");
 	}
 
 }
--- src/main/java/de/thowl/klimaralley/server/web/api/wasserarm/WasserAPI.java
+++ src/main/java/de/thowl/klimaralley/server/web/api/wasserarm/WasserAPI.java
@@ -11,19 +11,26 @@ import org.springframework.web.bind.annotation.RestController;
 import de.thowl.klimaralley.server.core.services.wasserarm.WasserarmService;
 import de.thowl.klimaralley.server.core.utils.auth.JWTtokenizer;
 import de.thowl.klimaralley.server.storage.entities.wasserarm.Eater;
+import de.thowl.klimaralley.server.storage.entities.wasserarm.WasserarmShopItem;
 import io.jsonwebtoken.Claims;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 
 /**
  * RESTful API of the Wasserarm satt game.
+ *
  * This API is a stub and subject to change.
+ *
+ * @author Jonas Schwind
+ * @version 0.6.0
  */
 @Slf4j
 @RestController
@@ -37,16 +44,11 @@ public class WasserAPI {
 	/**
 	 * Healthcheck method to 'ping' the API
 	 *
-	 * curl -X 'GET' 'http://localhost:8080/water/'
-	 *
 	 * @return status code {@code 200}
 	 */
 	@Operation(summary = "Healthcheck")
 	@ApiResponses(value = {
-		@ApiResponse(
-			responseCode = "200",
-			description = "Can be used to check if api is up",
-			content = @Content),
+		@ApiResponse(responseCode = "200", description = "Can be used to check if api is up"),
 	})
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ResponseEntity<Object> healthCheck() {
@@ -60,16 +62,15 @@ public class WasserAPI {
 	 *
 	 * curl -X 'GET' 'http://localhost:8080/water/items'
 	 * 
-	 * @return status code {@code 501} // TODO: Delete
-	 * @return repsonse (code {@code 200}) with all shop items as JSON
+	 * @return {@link WasserarmShopItem}[] (code {@code 200 OK})
 	 */
 	@Operation(summary = "Retrieves all shop items from the Database")
 	@ApiResponses(value = {
-		@ApiResponse(
-			responseCode = "200", 
-			description = "Wasserarmsatt shopitems as json", 
-			content = @Content),
-		})
+		@ApiResponse(responseCode = "200", 
+			description = "Wasserarmsatt shop items", 
+			content = @Content(mediaType = "application/json",
+				array = @ArraySchema(schema = @Schema(implementation = WasserarmShopItem.class)))),
+	})
 	@RequestMapping(value = "/items", method = RequestMethod.GET)
 	public ResponseEntity<Object> getAllItems() {
 		log.info("entering getAllItems (GET-Method: /water/items)");
@@ -85,9 +86,11 @@ public class WasserAPI {
 	 */
 	@Operation(summary = "Retrieves a Eater to serve")
 	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200",description = "Wasserarmsatt Eaater information as json", content = @Content),
-		}
-	)
+		@ApiResponse(responseCode = "200",
+			description = "Wasserarmsatt Eater information",
+			content = @Content(mediaType = "application/json",
+				schema = @Schema(implementation = Eater.class))),
+	})
 	@RequestMapping(value = "/eater", method = RequestMethod.GET)
 	public ResponseEntity<Object> getEater() {
 		
@@ -107,9 +110,9 @@ public class WasserAPI {
 	 */
     	@Operation(summary = "Retrieves game score based on items")
 	@ApiResponses(value = {
-		@ApiResponse(responseCode = "501", description = "Wasserarmsatt score", content = @Content)
+		@ApiResponse(responseCode = "501", description = "Wasserarmsatt score")
 	})
-	@RequestMapping(value = "/score", method = RequestMethod.GET)
+	@RequestMapping(value = "/score", method = RequestMethod.POST)
 	public ResponseEntity<Object> getScore(@Parameter(description = "JWT token") @RequestParam(required = false) String token) {
 
 		Claims claims;
