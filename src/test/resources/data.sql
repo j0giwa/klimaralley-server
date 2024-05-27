fa8ef5901a6dea61a50f0626c3e6bd7847ec5366
j0giwa <77389555+j0giwa@users.noreply.github.com>
2024-05-27 17:03:25 +0200

    Finaly fixed tha damn test

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO 
-	users(username, firstname, lastname, email, password, api_token) 
+	users(username, firstname, lastname, email, password) 
 VALUES 
-	('Rüdiger', 'Rüdiger', 'Schlablonzki', 'ruediger@thowl.de', '$2a$15$D3zQTzUx12LgCOHWOH8H4eSXbZ/lhO.l46wpR8vrA6qZ/uhOct..W', 'f7c7699e-6515-447e-9226-6c4d3fae4bfb');
+	('Rüdiger', 'Rüdiger', 'Schlablonzki', 'ruediger@thowl.de', '$2a$15$D3zQTzUx12LgCOHWOH8H4eSXbZ/lhO.l46wpR8vrA6qZ/uhOct..W');
