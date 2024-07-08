728afc3334398933c0c5271477c73edc51b3dfd1
Jonas Schwind <77389555+j0giwa@users.noreply.github.com>
2024-07-08 16:47:09 +0200

    Update WasserarmShopItemTypes

--- src/main/java/de/thowl/klimaralley/server/storage/entities/wasserarm/WasserarmShopItemType.java
+++ src/main/java/de/thowl/klimaralley/server/storage/entities/wasserarm/WasserarmShopItemType.java
@@ -1,6 +1,5 @@
 package de.thowl.klimaralley.server.storage.entities.wasserarm;
 
-
 /**
  * The type of an {@link WasserarmShopItem}.
  * The type signalises which items are legible for certeain {@link EaterDiet}'s
@@ -11,7 +10,9 @@ package de.thowl.klimaralley.server.storage.entities.wasserarm;
 public enum WasserarmShopItemType {
 	FRUIT,
 	VEGETABLE,
-	MEAT,
-	ANINAL_PRODUCT,
-	DRINK;
+	NUTS,
+	MEAT_AND_POULTRY,
+	FISH_AND_SEAFOOD,
+	DAIRY_FOODS,
+	DRINKS;
 }
